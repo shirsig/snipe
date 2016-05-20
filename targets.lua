bbd83684f7e18e38b41c74ee64a256792e4aa5e0
Manuel Simon Hirsig <simon.hirsig@gmail.com>
2016-05-20 12:11:29 +0200

    changed example targets

--- targets.lua
+++ targets.lua
@@ -1,16 +1,4 @@
 snipe.target_sets = {
-	kronos2 = {
-		{ g=1, "scouting gloves of the monkey", "+4 agility", "+4 stamina" },
-		{ g=1, "wrangler's wristbands of the monkey", "+3 agility", "+3 stamina" },
-		{ g=3, "hook dagger of fiery wrath", "+4 fire spell damage" },
-		{ g=3, "shimmering sash of fiery wrath", "+9 fire spell damage" },
-		{ g=3, "greenweave bracers of fiery wrath", "+6 fire spell damage" },
-		{ g=3, "pagan mitts of fiery wrath", "+9 fire spell damage" },
-		{ g=1, "watcher's cape", "+7 fire spell damage" },
-		-- { g=1, "sentry's cape", "+7 fire spell damage" },
-		{ g=3, "shimmering trousers of fiery wrath", "+11 fire spell damage" },
-	},
-
 	kronos = {
 		{ g=100, "alcor's sunrazor" },
 		{ g=100, "edgemaster's handguards" },
@@ -18,7 +6,20 @@ snipe.target_sets = {
 		{ g=30, "wand", "+23 fire spell damage" },
 		{ g=30, "wand", "+23 shadow spell damage" },
 		{ g=30, "wand", "+33 healing spells" },
+		{ g=10, "shadowfang" },
 		{ g=4, "scouting gloves of the monkey", "+4 agility", "+4 stamina" },
 		{ g=4, "wrangler's wristbands of the monkey", "+3 agility", "+3 stamina" },
+
+		{ c=25, "exploding shot" },
 	},
+
+	twinkfire = {
+		{ g=1, "hook dagger of fiery wrath", "+4 fire spell damage" },
+		{ g=1, "shimmering sash of fiery wrath", "+9 fire spell damage" },
+		{ g=1, "greenweave bracers of fiery wrath", "+6 fire spell damage" },
+		{ g=1, "pagan mitts of fiery wrath", "+9 fire spell damage" },
+		{ g=1, "watcher's cape", "+7 fire spell damage" },
+		{ g=1, "sentry's cape", "+7 fire spell damage" },
+		{ g=1, "shimmering trousers of fiery wrath", "+11 fire spell damage" },
+	}
 }
